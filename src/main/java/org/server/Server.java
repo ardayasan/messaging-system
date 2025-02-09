377e042fa71af0d99d31ec813c799ac1b21ffba9
Arda Yasan <a.yasan2021@gtu.edu.tr>
2025-02-09 23:36:26 +0300

    fix

--- src/main/java/org/server/Server.java
+++ src/main/java/org/server/Server.java
@@ -6,4 +6,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Server {
+    private static final int PORT = 12345;
+    private static Map<String, ClientHandler> clients = new HashMap<>();
+
+    public static void main(String[] args) {
+        System.out.println("Server başlatılıyor...");
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(socket, clients);
+                clientHandler.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
