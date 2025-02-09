135bd116f4ff6a365098da69f5e6ff26ff58070c
Arda Yasan <a.yasan2021@gtu.edu.tr>
2025-02-09 23:35:41 +0300

    gui1

--- src/main/java/org/client/ClientGUI.java
+++ src/main/java/org/client/ClientGUI.java
@@ -1,5 +1,17 @@
 package org.client;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ClientGUI {
     private JFrame frame;
     private JTextField messageField;
